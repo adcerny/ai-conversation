508d002f7633d812845b6acdaed25d63d273280b
Adam Cerny <ACerny@christies.com>
2025-12-01 09:28:22 +0000

    Added options for subject, model and rounds

--- SubjectConfig.cs
+++ SubjectConfig.cs
@@ -2,7 +2,5 @@ using System.Collections.Generic;
 
 internal class SubjectConfig
 {
-    public int NumberOfRounds { get; set; }
-
     public Dictionary<string, ChatModelConfig> Models { get; set; } = new();
 }
